85916bf725c16c3a8b3916578c06fbe238fd7b88
skytrain <paris110011@gmail.com>
2019-12-29 16:24:48 +0900

    upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function(){
+    var header = document.getElementById("header");
+    var menuToggle = document.getElementById("toggle");
+    
+    menuToggle.addEventListener("click", function(){
+        if(header.className === "open"){
+            header.className = "";
+        }else{
+            header.className = "open";
+        }
+    })
+})();
